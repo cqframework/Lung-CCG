9dca3384e1da6d02f6f1c133b1adec00f993e8cc
ROBIN\Kurt <KurtA@Gjoll.com>
2020-05-22 11:37:04 -0500

    Rework project directories, added MFsh and tools.

---
+++ FSH-LungCCG/BuildIG.bat
@@ -0,0 +1,3 @@
+pushd build
+_genonce.bat
+popd
---
+++ FSH-LungCCG/InstallTools.bat
@@ -0,0 +1,12 @@
+dotnet tool uninstall --global Eir.AutoMate
+dotnet tool uninstall --global Eir.MFSH
+dotnet tool uninstall --global Eir.FGraph
+dotnet tool uninstall --global Eir.IGBuilder
+
+
+dotnet tool install --global Eir.AutoMate --version 1.4.0
+dotnet tool install --global Eir.MFSH --version 2.0.5
+dotnet tool install --global Eir.FGraph --version 2.0.0
+dotnet tool install --global Eir.IGBuilder --version 1.4.3
+
+pause
