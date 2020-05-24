54e9a1ceb3115369253a5d4d520eec943b2403ad
ROBIN\Kurt <KurtA@Gjoll.com>
2020-05-24 12:11:39 -0500

    Updated instal tool batch file.

--- FSH-LungCCG/InstallTools.bat
+++ FSH-LungCCG/InstallTools.bat
@@ -1,12 +1,13 @@
+dotnet nuget locals all --clear
+
 dotnet tool uninstall --global Eir.AutoMate
 dotnet tool uninstall --global Eir.MFSH
-dotnet tool uninstall --global Eir.FGraph
-dotnet tool uninstall --global Eir.IGBuilder
+rem dotnet tool uninstall --global Eir.FGraph
+rem dotnet tool uninstall --global Eir.IGBuilder
 
-
-dotnet tool install --global Eir.AutoMate --version 1.4.0
-dotnet tool install --global Eir.MFSH --version 2.0.6
-dotnet tool install --global Eir.FGraph --version 2.0.0
-dotnet tool install --global Eir.IGBuilder --version 1.4.3
+dotnet tool install --global Eir.AutoMate
+dotnet tool install --global Eir.MFSH
+rem dotnet tool install --global Eir.FGraph
+rem dotnet tool install --global Eir.IGBuilder
 
 pause
