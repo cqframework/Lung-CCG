acf0ecee12e9046d9f73073a9f20fbe72d110dca
goelalex <goelalex@users.noreply.github.com>
2020-09-15 14:59:19 -0400

    Comments
    Comments so Alex remembers what to do

--- FSH-LungCCG/AutoMate.bat
+++ FSH-LungCCG/AutoMate.bat
@@ -1,2 +1,3 @@
 AutoMate.exe
 
+rem runs AutoMate.json - watches existing files to re-run when changes 
--- FSH-LungCCG/InstallTools.bat
+++ FSH-LungCCG/InstallTools.bat
@@ -8,6 +8,10 @@ rem dotnet tool uninstall --global Eir.IGBuilder
 dotnet tool install --global Eir.AutoMate
 dotnet tool install --global Eir.MFSH
 rem dotnet tool install --global Eir.FGraph
+rem Builds graph
 rem dotnet tool install --global Eir.IGBuilder
+rem Automatically compiles IG 
 
 pause
+rem Goes to nuget and gets the latest tools that are out there.
+rem FSH toool that compiles the FSH
